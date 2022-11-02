eae918bed2061a17ee33b8fe2f08d96d40893da5
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-02 16:20:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var PORT = 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function(){
+    console.log("Server is now running");
+})
