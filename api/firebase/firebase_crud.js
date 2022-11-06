e9fdd8b8ab25591607efd2f06afa8b293d285d37
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-06 19:15:35 +0530

    new packages jwt and cookie-parser, file formated

--- api/firebase/firebase_crud.js
+++ api/firebase/firebase_crud.js
@@ -4,26 +4,21 @@ const { getDatabase } = require('firebase/database');
 require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY || "AIzaSyBGV1iXgRlJH9YJ5zFHF2GGCXUnjoI9F_Y",
-    authDomain: process.env.AUTH_ADMIN || "ssd-assignment-2-6723b.firebaseapp.com",
-    projectId: process.env.PROJECT_ID || "ssd-assignment-2-6723b",
-    storageBucket: process.env.STORAGE_BUCKET_ID || "ssd-assignment-2-6723b.appspot.com",
-    messagingSenderId: process.env.MESSAGING_SENDER_ID || "238802327118",
-    appId: process.env.APP_ID || "1:238802327118:web:2bd8771762755d9b1f551f",
-    measurementId: process.env.MESUREMENT_ID || "G-K303J998JX",
-    databaseURL: process.env.DATABES_URL || "https://ssd-assignment-2-6723b-default-rtdb.firebaseio.com/"
+    apiKey: "AIzaSyBGV1iXgRlJH9YJ5zFHF2GGCXUnjoI9F_Y",
+    authDomain: "ssd-assignment-2-6723b.firebaseapp.com",
+    projectId: "ssd-assignment-2-6723b",
+    storageBucket: "ssd-assignment-2-6723b.appspot.com",
+    messagingSenderId: "238802327118",
+    appId: "1:238802327118:web:2bd8771762755d9b1f551f",
+    measurementId: "G-K303J998JX",
+    databaseURL: "https://ssd-assignment-2-6723b-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const appFirebase = initializeApp(firebaseConfig);
 // Get a database reference to our blog
 const database = getDatabase(appFirebase);
-
-
 const storageRef = getStorage();
-
-
-
 
 module.exports = {
     storageRef,
