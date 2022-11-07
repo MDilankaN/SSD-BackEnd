2c2382c52a6ed00e0e089605918d2f013ff996c5
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-07 22:24:05 +0530

    verification middeleware added

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const {
     registerUser, loginUser
 } = require("../controllers/userController");
+const verifyJWT = require('../middleware/verifyJWT')
 
-router.post("/register", registerUser);
+router.post("/register",verifyJWT, registerUser);
 router.post("/login", loginUser);
 
 module.exports = router;
