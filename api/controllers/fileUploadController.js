6e2fc1af49621dfc56c3d2f1b0938a03af6599c0
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2022-11-18 14:46:27 +0530

    changes updated
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- api/controllers/fileUploadController.js
+++ api/controllers/fileUploadController.js
@@ -18,32 +18,7 @@ const addFiles = async (req, res) => {
     try {
       let files = req.files;
       console.log(req.files);
-
-      // uploadTask.on(
-      //     "state_changed",
-      //     (snapshot) => {
-      //         const uploaded = Math.floor(
-      //             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      //         );
-      //     },
-      //     (error) => {
-      //         console.log(error);
-      //     },
-      //     () => {
-      //
-      //     }
-      // );
-
-      //   Object.keys(files).forEach((key) => {
-      //     const filepath = path.join(__dirname, "files", files[key].name);
-
-      //     files[key].mv(filepath, (err) => {
-      //       if (err) res.status(500).send(err);
-      //     });
-      //   });
-
-      //   console.log(__dirname+'\\'+files.file.name);
-
+      
       const storageRef = ref(storage, "FileUpload/" + files.file.name);
 
       uploadBytes(storageRef, files.file.data).then((snapshot) => {
@@ -52,11 +27,7 @@ const addFiles = async (req, res) => {
       res.send({
         status: true,
         message: "File is uploaded",
-        // data: {
-        //   name: fileUpload.name,
-        //   mimetype: fileUpload.mimetype,
-        //   size: fileUpload.size,
-        // },
+
       });
     } catch (err) {
       console.log(err);
